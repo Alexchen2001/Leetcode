fa9b5d7d5dc8823a8d057e9bd3e12261a8dc550d
Alexchen2001 <alexchen9899627@gmail.com>
2024-06-06 21:30:59 -0700

    Leetcode Easy: DFS

---
+++ 111_Minimum Depth of Binary Tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int: 
+        def dfs(root):
+            if not root:
+                return 0
+            if root.left is None:
+                return 1 + dfs(root.right)
+            if root.right is None:
+                return 1 + dfs(root.left)
+            return 1 + min(dfs(root.left), dfs(root.right))
+        
+        return dfs(root)
+
