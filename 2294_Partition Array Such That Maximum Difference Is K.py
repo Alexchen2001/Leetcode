24f7055c3cef48dde3a6a456640e90073f1ade01
Alexchen2001 <alexchen9899627@gmail.com>
2024-08-18 21:58:43 -0700

    Leetcode Medium: Greedy Algo

---
+++ 2294_Partition Array Such That Maximum Difference Is K.py
@@ -0,0 +1,14 @@
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = 1
+        x = nums[0]
+
+        for i in range (1,len(nums)):
+            if nums[i] - x > k:
+                x = nums[i]
+                ans += 1
+        
+        return ans
+                
+        
