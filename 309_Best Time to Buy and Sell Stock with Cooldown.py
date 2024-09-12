4ac0e3b0178e0406d550a746e9c184d7e44ef092
Alexchen2001 <72245112+Alexchen2001@users.noreply.github.com>
2024-09-11 17:03:59 -0700

    Leetcode Medium: DP Top-down

---
+++ 309_Best Time to Buy and Sell Stock with Cooldown.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        @cache
+        def dp(index, holdStock):
+            if index >= len(prices):
+                return 0
+            
+            ans = dp(index + 1, holdStock)
+            if holdStock:
+                ans = max(ans, prices[index] + dp(index + 2, False))
+            else:
+                ans = max(ans, -prices[index] + dp(index + 1, True))
+            
+            return ans
+        return dp(0,False)
+
+
+        
