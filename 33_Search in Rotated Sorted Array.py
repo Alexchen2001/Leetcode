083e97e70d029c90a2edb99982eb299d89f5e4f9
Alexchen2001 <alexchen9899627@gmail.com>
2025-03-03 23:37:10 -0800

    Leetcode Medium Revised: Binary Search O(log n) only

--- 33_Search in Rotated Sorted Array.py
+++ 33_Search in Rotated Sorted Array.py
@@ -1,19 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        prev = nums[0]
-        pivot = 0
+        initial = nums[0]
         n = len(nums)
-
-        for i in range(1, n):
-            if nums[i] < prev:
-                pivot = i
-                break
+        pivot = n -1
+        low, high = 0, n -1
+        if nums[0] > nums[n - 1]:
+            while low <= high:
+                mid = (low + high) // 2
+                print(low, high, mid, nums[mid])
+                if nums[mid] > nums[mid + 1]:
+                    pivot = mid
+                    break
+                elif nums[mid] >= initial:
+                    low = mid + 1
+                else:
+                    high = mid - 1
         print(pivot)
         low = 0
         high = n - 1
         while low <= high:
             mid = (low + high) // 2
-            realIndex = (mid + pivot) % n
+            realIndex = (mid + pivot + 1) % n
             if nums[realIndex] == target:
                 return realIndex
             elif nums[realIndex] < target:
